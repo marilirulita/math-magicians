86984e9dab50ab8924dccb4d8ede9d837fc57753
marysol <marilirulita@gmail.com>
2022-02-08 17:01:46 -0800

    Add Calculator class to create board

---
+++ src/components/Calculator.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import './Calculator.css';
+
+class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="Container">
+        <div className="Calculator">
+          <div className="Result">0</div>
+          <div className="Row">
+            <div className="Number">AC</div>
+            <div className="Number">+/-</div>
+            <div className="Number">%</div>
+            <div className="Simbol">/</div>
+          </div>
+          <div className="Row">
+            <div className="Number">7</div>
+            <div className="Number">8</div>
+            <div className="Number">9</div>
+            <div className="Simbol">x</div>
+          </div>
+          <div className="Row">
+            <div className="Number">4</div>
+            <div className="Number">5</div>
+            <div className="Number">6</div>
+            <div className="Simbol">-</div>
+          </div>
+          <div className="Row">
+            <div className="Number">1</div>
+            <div className="Number">2</div>
+            <div className="Number">3</div>
+            <div className="Simbol">+</div>
+          </div>
+          <div className="Row">
+            <div className="Special">
+              <div className="Number">0</div>
+            </div>
+            <div className="Special">
+              <div className="Number">.</div>
+              <div className="Simbol">=</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Calculator;
