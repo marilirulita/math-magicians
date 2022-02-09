bedcd8f270208bb5f734320dcc2e8d29af66b5bd
marysol <marilirulita@gmail.com>
2022-02-08 17:40:31 -0800

    Fix style linters

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -8,40 +8,40 @@ class Calculator extends React.Component {
 
   render() {
     return (
-      <div className="Container">
-        <div className="Calculator">
-          <div className="Result">0</div>
-          <div className="Row">
-            <div className="Number">AC</div>
-            <div className="Number">+/-</div>
-            <div className="Number">%</div>
-            <div className="Simbol">/</div>
+      <div className="container">
+        <div className="calculator">
+          <div className="result">0</div>
+          <div className="row">
+            <div className="number">AC</div>
+            <div className="number">+/-</div>
+            <div className="number">%</div>
+            <div className="simbol">/</div>
           </div>
-          <div className="Row">
-            <div className="Number">7</div>
-            <div className="Number">8</div>
-            <div className="Number">9</div>
-            <div className="Simbol">x</div>
+          <div className="row">
+            <div className="number">7</div>
+            <div className="number">8</div>
+            <div className="number">9</div>
+            <div className="simbol">x</div>
           </div>
-          <div className="Row">
-            <div className="Number">4</div>
-            <div className="Number">5</div>
-            <div className="Number">6</div>
-            <div className="Simbol">-</div>
+          <div className="row">
+            <div className="number">4</div>
+            <div className="number">5</div>
+            <div className="number">6</div>
+            <div className="simbol">-</div>
           </div>
-          <div className="Row">
-            <div className="Number">1</div>
-            <div className="Number">2</div>
-            <div className="Number">3</div>
-            <div className="Simbol">+</div>
+          <div className="row">
+            <div className="number">1</div>
+            <div className="number">2</div>
+            <div className="number">3</div>
+            <div className="simbol">+</div>
           </div>
-          <div className="Row">
-            <div className="Special">
-              <div className="Number">0</div>
+          <div className="row">
+            <div className="special">
+              <div className="number">0</div>
             </div>
-            <div className="Special">
-              <div className="Number">.</div>
-              <div className="Simbol">=</div>
+            <div className="special">
+              <div className="number">.</div>
+              <div className="simbol">=</div>
             </div>
           </div>
         </div>
