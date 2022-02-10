1ae8f6001570494546f32f2f828b82c63bd2aee4
marysol <marilirulita@gmail.com>
2022-02-09 23:17:39 -0800

    Add state and setstate to calculator class

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,49 +1,70 @@
 import React from 'react';
-// import operate from '../logic/operate';
 import calculate from '../logic/calculate';
 
 class Calculator extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+    this.clickFunction = this.clickFunction.bind(this);
+  }
+
+  updateState(objx) {
+    this.setState(objx);
+  }
+
+  clickFunction(e) {
+    const button = e.target.innerHTML;
+    const answer = calculate(this.state, button);
+    this.updateState(answer);
   }
 
   render() {
+    const { total, operation, next } = this.state;
     return (
       <div className="container">
         <div className="calculator">
-          <div className="result">0</div>
-          <div className="row">
-            <div className="number">AC</div>
-            <div className="number">+/-</div>
-            <div className="number">%</div>
-            <div className="simbol">÷</div>
+          <div className="result">
+            {total}
+            {' '}
+            {operation}
+            {' '}
+            {next}
           </div>
           <div className="row">
-            <div className="number">7</div>
-            <div className="number">8</div>
-            <div className="number">9</div>
-            <div className="simbol">x</div>
+            <div className="number" onClick={this.clickFunction}>AC</div>
+            <div className="number" onClick={this.clickFunction}>+/-</div>
+            <div className="number" onClick={this.clickFunction}>%</div>
+            <div className="simbol" onClick={this.clickFunction}>÷</div>
           </div>
           <div className="row">
-            <div className="number">4</div>
-            <div className="number">5</div>
-            <div className="number">6</div>
-            <div className="simbol">-</div>
+            <div className="number" onClick={this.clickFunction}>7</div>
+            <div className="number" onClick={this.clickFunction}>8</div>
+            <div className="number" onClick={this.clickFunction}>9</div>
+            <div className="simbol" onClick={this.clickFunction}>x</div>
           </div>
           <div className="row">
-            <div className="number">1</div>
-            <div className="number">2</div>
-            <div className="number">3</div>
-            <div className="simbol">+</div>
+            <div className="number" onClick={this.clickFunction}>4</div>
+            <div className="number" onClick={this.clickFunction}>5</div>
+            <div className="number" onClick={this.clickFunction}>6</div>
+            <div className="simbol" onClick={this.clickFunction}>-</div>
+          </div>
+          <div className="row">
+            <div className="number" onClick={this.clickFunction}>1</div>
+            <div className="number" onClick={this.clickFunction}>2</div>
+            <div className="number" onClick={this.clickFunction}>3</div>
+            <div className="simbol" onClick={this.clickFunction}>+</div>
           </div>
           <div className="row">
             <div className="row">
-              <div className="special">0</div>
+              <div className="special" onClick={this.clickFunction}>0</div>
             </div>
             <div className="row">
-              <div className="number">.</div>
-              <div className="simbol">=</div>
+              <div className="number" onClick={this.clickFunction}>.</div>
+              <div className="simbol" onClick={this.clickFunction}>=</div>
             </div>
           </div>
         </div>
